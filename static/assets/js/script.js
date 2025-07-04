016fb2edf5e8a3cce4559ec946049f1cef971e4a
Francisco Itao Tovar <fitaot@clinicatierrasanta.local>
2025-07-04 15:25:32 -0400

    index titulos y mejoras visuales

--- static/assets/js/script.js
+++ static/assets/js/script.js
@@ -1,3 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
     console.log("Sitio activo, listo para deslizar anuncios 🎉");
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const lineas = document.querySelectorAll('#hero-heading .linea');
+    lineas.forEach((linea, index) => {
+        setTimeout(() => {
+            linea.classList.add('visible');
+        }, index * 800);
+    });
+});
